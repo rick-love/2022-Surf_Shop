e601e8635220bfe29842ca69488268647231f67e
rick-love <ricklove@outlook.com>
2022-03-10 21:48:50 +0100

    adding a theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,24 @@ import ProductScreen from './screens/ProductScreen'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+
+import { ThemeProvider } from '@mui/material'
+import themeOptions from "./assets/theme";
+
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main className='container mx-auto border-2 border-solid'>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} />
-          <Route path='/product/:id' element={<ProductScreen />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={themeOptions}>
+      <Router>
+        <Header />
+        <main className='container mx-auto border-2 border-solid'>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   )
 }
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -15,9 +15,13 @@ import {
   Fab,
   ListItem,
   Stack,
+  ThemeProvider,
   Typography,
 } from '@mui/material'
+
 import { listProductDetails } from '../actions/productActions'
+import themeOptions from "../assets/theme";
+
 
 const ProductScreen = () => {
   const params = useParams()
@@ -30,63 +34,65 @@ const ProductScreen = () => {
   }, [dispatch, params])
 
   return (
-    <Container>
-      <Fab variant='extended'>
-        <Link to='/'>Go Back</Link>
-      </Fab>
+    <ThemeProvider theme={themeOptions}>
+      <Container>
+        <Fab variant='extended'>
+          <Link to='/'>Go Back</Link>
+        </Fab>
 
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message>{error}</Message>
-      ) : (
-        <Card sx={{ display: 'flex' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <CardContent>
-              <Typography gutterBottom variant='h2' component='div'>
-                {product.name}
-              </Typography>
-              <Typography gutterBottom color='orange'>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </Typography>
-              <Typography variant='body2' color='text-secondary'>
-                {product.description}
-              </Typography>
-              <Typography variant='h5' component='div'>
-                {product.price}
-              </Typography>
-            </CardContent>
-            <Box>
-              <Stack
-                sx={{
-                  border: 1,
-                }}
-                direction={{ xs: 'column' }}
-                spacing={{ xs: 1, sm: 2, md: 4 }}
-                divider={<Divider orientation='horizontal' flexItem />}
-              >
-                <ListItem>In Stock: {product.countInStock}</ListItem>
-                <ListItem>
-                  <Fab variant='extended'>
-                    <Link to='/'>Add to Cart</Link>
-                  </Fab>
-                </ListItem>
-                <ListItem>ListItem 3</ListItem>
-              </Stack>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message>{error}</Message>
+        ) : (
+          <Card sx={{ display: 'flex' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <CardContent>
+                <Typography gutterBottom variant='h2' component='div'>
+                  {product.name}
+                </Typography>
+                <Typography gutterBottom color='orange'>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </Typography>
+                <Typography variant='body2' color='text-secondary'>
+                  {product.description}
+                </Typography>
+                <Typography variant='h5' component='div'>
+                  {product.price}
+                </Typography>
+              </CardContent>
+              <Box>
+                <Stack
+                  sx={{
+                    border: 1,
+                  }}
+                  direction={{ xs: 'column' }}
+                  spacing={{ xs: 1, sm: 2, md: 4 }}
+                  divider={<Divider orientation='horizontal' flexItem />}
+                >
+                  <ListItem>In Stock: {product.countInStock}</ListItem>
+                  <ListItem>
+                    <Fab variant='extended'>
+                      <Link to='/'>Add to Cart</Link>
+                    </Fab>
+                  </ListItem>
+                  <ListItem>ListItem 3</ListItem>
+                </Stack>
+              </Box>
             </Box>
-          </Box>
-          <CardMedia
-            component='img'
-            height='140'
-            image={product.image}
-            alt='product'
-          />
-        </Card>
-      )}
-    </Container>
+            <CardMedia
+              component='img'
+              height='140'
+              image={product.image}
+              alt='product'
+            />
+          </Card>
+        )}
+      </Container>
+    </ThemeProvider>
   )
 }
 
